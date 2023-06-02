a5e9737aa9c0da5a88020155acc4580b91fa8d1c
Fernando Barbosa <fernando.barbosa.fb14@gmail.com>
2023-06-02 14:55:22 -0300

    Salvando arquivo no repositorio

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
+const path = require('path')
+
 const checklistRouter = require('./src/routes/checklist.js')
+const rootRouter = require('./src/routes/index.js')
+
 require('./config/database.js')
+
 
 const app = express()
 app.use(express.json())
+
+app.use('/', rootRouter)
 app.use('/checklists', checklistRouter)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Minha lista de tarefas</h1>')
-})
-
-app.get('/json', (req, res) => {
-  res.json({ title: 'Tarefa X', done: true })
-})
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
 
 app.listen(3003, () => {
   console.log('Servidor foi iniciado...')
