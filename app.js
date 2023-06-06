fb8faf4f3953b74ef4d448ecf4000e0d2e18830a
Fernando Barbosa <fernando.barbosa.fb14@gmail.com>
2023-06-06 17:46:08 -0300

    Salvando arquivo no repositorio

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('./config/database.js')
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
